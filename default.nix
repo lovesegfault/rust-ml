c1c6262f2dda4fe87e5b01e06ef3a65be5548768
Bernardo Meurer <bernardo@standard.ai>
2019-04-06 21:57:16 -0700

    Fixed Nix shell

--- default.nix
+++ default.nix
@@ -1,24 +1,15 @@
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-
-  rustNightlyChannel = nixpkgs.rustChannelOf { date = "2019-02-27"; channel = "nightly"; };
-  rustNightly = rustNightlyChannel.rust.override {
-    extensions = [
-      "rust-src"
-      "rls-preview"
-      "clippy-preview"
-      "rustfmt-preview"
-    ];
-  };
-
-  daedalos = with nixpkgs;
-  stdenv.mkDerivation {
+  nixpkgs = import <nixpkgs> {};
+  rustML = with nixpkgs; stdenv.mkDerivation {
     name = "rust-ml";
     buildInputs = [
-      rustNightly
+      cargo
+      (python3.withPackages(ps: with ps; [
+        ps.tensorflowWithCuda
+      ]))
+
     ];
   };
 in {
-  inherit daedalos nixpkgs rustNightly;
+  inherit rustML nixpkgs;
 }
--- shell.nix
+++ shell.nix
@@ -1,1 +1,1 @@
-(import ./.).rust-ml
+(import ./.).rustML
