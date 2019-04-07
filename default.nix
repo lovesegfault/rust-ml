1f829da6e9bca1c4435ebee8ae44322406bdc0d7
Bernardo Meurer <bernardo@standard.ai>
2019-04-07 01:38:53 -0700

    Finished base impl (no output yet)

--- default.nix
+++ default.nix
@@ -1,14 +1,29 @@
 let
-  nixpkgs = import <nixpkgs> {};
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = nixpkgs.latest.rustChannels.stable;
+  rustStable = rustChannel.rust.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+      "clippy-preview"
+      "rustfmt-preview"
+    ];
+  };
   rustML = with nixpkgs; stdenv.mkDerivation {
     name = "rust-ml";
     buildInputs = [
-      cargo
+      rustStable
+      gcc-unwrapped.lib
+      openssl
+      pkgconfig
       (python3.withPackages(ps: with ps; [
         ps.tensorflowWithCuda
       ]))
-
     ];
+    shellHook = ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${nixpkgs.gcc-unwrapped.lib}/lib"
+    '';
   };
 in {
   inherit rustML nixpkgs;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,91 @@
-fn main() {
-    println!("Hello, world!");
+use image::GenericImageView;
+use itertools::Itertools;
+use structopt::StructOpt;
+use tensorflow::{Graph, ImportGraphDefOptions, Session, SessionOptions, SessionRunArgs, Tensor};
+
+use std::error::Error;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+    #[structopt(parse(from_os_str))]
+    output: PathBuf,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct BBox {
+    pub y1: f32,
+    pub x1: f32,
+    pub y2: f32,
+    pub x2: f32,
+    pub prob: f32,
+}
+
+macro_rules! flatten_hack {
+    ($x: expr) => (std::iter::once($x));
+    ($x: expr, $($y: expr),+) => (flatten_hack!($x).chain(flatten_hack!($($y),+)));
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Parse otps
+    let opt = Opt::from_args();
+    // Open image
+    let input_image = image::open(&opt.input)?;
+    // Load model
+    let model = include_bytes!("../models/mtcnn.pb");
+    let mut graph = Graph::new();
+    graph.import_graph_def(&*model, &ImportGraphDefOptions::new())?;
+    // Flatten image
+    let flattened: Vec<f32> = input_image
+        .pixels()
+        .flat_map(|(_, _, rgb)| flatten_hack!(rgb[2], rgb[1], rgb[0]))
+        .map(|i| i as f32)
+        .collect();
+    // Load image into a tensor
+    let input = Tensor::new(&[input_image.height() as u64, input_image.width() as u64, 3])
+        .with_values(&flattened)?;
+    let mut session = Session::new(&SessionOptions::new(), &graph)?;
+    // mtcnn model inputs
+    let min_size = Tensor::new(&[]).with_values(&[40_f32])?;
+    let thresholds = Tensor::new(&[3]).with_values(&[0.6_f32, 0.7_f32, 0.7_f32])?;
+    let factor = Tensor::new(&[]).with_values(&[0.709_f32])?;
+    // Load model parameters
+    let mut args = SessionRunArgs::new();
+    args.add_feed(&graph.operation_by_name_required("min_size")?, 0, &min_size);
+    args.add_feed(
+        &graph.operation_by_name_required("thresholds")?,
+        0,
+        &thresholds,
+    );
+    args.add_feed(&graph.operation_by_name_required("factor")?, 0, &factor);
+    // Load input image
+    args.add_feed(&graph.operation_by_name_required("input")?, 0, &input);
+    // Set outputs
+    let bbox = args.request_fetch(&graph.operation_by_name_required("box")?, 0);
+    let prob = args.request_fetch(&graph.operation_by_name_required("prob")?, 0);
+    // Run session
+    session.run(&mut args)?;
+    // Save results
+    let bbox_res: Tensor<f32> = args.fetch(bbox)?;
+    let prob_res: Tensor<f32> = args.fetch(prob)?;
+    // Vectorize bboxes
+    let bboxes: Vec<BBox> = bbox_res
+        .into_iter()
+        .tuples::<(_, _, _, _)>() // Chunk the iterator into 4-tuples
+        .zip(prob_res.into_iter()) // Zip it with the probabilities
+        .map(|((y1, x1, y2, x2), prob)| {
+            // Map values into struct
+            BBox {
+                y1: *y1,
+                x1: *x1,
+                y2: *y2,
+                x2: *x2,
+                prob: *prob,
+            }
+        })
+        .collect();
+    println!("BBox Length: {}, BBoxes: {:#?}", bboxes.len(), bboxes);
+    Ok(())
 }
